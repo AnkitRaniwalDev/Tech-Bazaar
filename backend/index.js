7905ccf0058dab8d9c1fa7f273f2b92dedd415d4
AnkitRaniwalDev <ankit.alw011@gmail.com>
2026-01-13 12:17:51 +0530

    final cors fix

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,7 @@ import  adminRouter  from "./src/routes/admin.route.js";
 
 dotenv.config();
 const app =express();
-app.use(cors({
-  origin: "*", 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 app.use (express.json());
 
 
